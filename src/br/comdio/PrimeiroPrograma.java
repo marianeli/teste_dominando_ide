7374edd6f757bff59175a2788b9d46100a6eec9d
Mariane <marianelimaamorim@gmail.com>
2021-08-18 17:11:20 -0300

    first commit

---
+++ src/br/comdio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.comdio;
+
+import br.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		/*Dint a = 2;
+		int b = 3;
+		System.out.println("Hello World");*/
+
+	}
+
+}
